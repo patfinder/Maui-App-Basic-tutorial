0732cf345afdd70473b7374d81e7347822ab24da
patfinder <vuong_se@yahoo.com>
2025-01-18 22:34:53 -0500

    Add models and model binding

--- MauiApp2/Views/AboutPage.xaml.cs
+++ MauiApp2/Views/AboutPage.xaml.cs
@@ -7,8 +7,12 @@ public partial class AboutPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void LearnMore_Clicked(object sender, EventArgs e)
-    {
-        DisplayAlert("Clicked!", "You clicked the button!", "OK");
-    }
+	private async void LearnMore_Clicked(object sender, EventArgs e)
+	{
+		if (BindingContext is Models.About about)
+		{
+			// Navigate to the specified URL in the system browser.
+			await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+		}
+	}
 }
